9d92923438e118836f6d5ea963bad61b46851636
biplab21 <biplabpaul569@gmail.com>
2024-03-09 13:28:19 +0530

    Chapter 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,31 +1,41 @@
-import { useState } from "react"
-import resList from "../utils/mockData"
+import { useEffect, useState } from "react"
 import ResturantCard from "./ResturantCard"
+import Shimmer from "./Shimmer"
 
 const Body = () => {
 
-    let [ listOfResturants, setListOfResturants ] = useState(resList)
-    let [searchKeyword, setSearchKeyword] = useState("")
+    let [ listOfResturants, setListOfResturants ] = useState([])
+    let [ filteredResturants, setFilteredResturants ] = useState([])
+    let [ searchKeyword, setSearchKeyword ] = useState("")
 
-    const handleSearch=(e)=>{
-        const keyword = e.target.value
-        setSearchKeyword(keyword)
-        const searchedItem = keyword !== "" ? resList.filter((res)=>res.info.name.toLowerCase().includes(searchKeyword.toLowerCase())): resList
-        setListOfResturants(searchedItem)
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json()
+        setListOfResturants(json?.data?.cards[ 4 ]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        setFilteredResturants(json?.data?.cards[ 4 ]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
 
-    return (
+    //Conditional rendering
+    return listOfResturants.length === 0 ? <Shimmer /> : (
         <div className="body">
-            <div className="search">
-                <input
-                    type="text"
-                    className="searchBox"
-                    placeholder="Search Resturants" 
-                    onChange={handleSearch}
-                    value={searchKeyword}
-                />
-            </div>
             <div className="filter">
+                <div className="search">
+                    <input
+                        type="text"
+                        className="search-box"
+                        placeholder="Search Resturants"
+                        onChange={ (e) => setSearchKeyword(e.target.value) }
+                        value={ searchKeyword }
+                    />
+                    <button onClick={ () => {
+                        const filteredRes = listOfResturants.filter((res) => res.info.name.toLowerCase().includes(searchKeyword.toLowerCase()))
+                        setFilteredResturants(filteredRes)
+                    } }>Search</button>
+                </div>
                 <button
                     className="filter-btn"
                     onClick={ () => {
@@ -38,7 +48,7 @@ const Body = () => {
             </div>
             <div className="res-container">
                 {
-                    listOfResturants.map((resturant) => (
+                    filteredResturants.map((resturant) => (
                         <ResturantCard key={ resturant.info.id } resData={ resturant } />
                     ))
                 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+
+    const [ btnName, setBtnName ] = useState("Login")
+
     return (
         <div className="header">
             <div className='logo-container'>
-                <img className='logo' src={LOGO_URL} alt="logo" />
+                <img className='logo' src={ LOGO_URL } alt="logo" />
             </div>
             <div className="nav-items">
                 <ul>
@@ -12,6 +16,12 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button
+                        onClick={ () => {
+                            btnName === "Login"? setBtnName("Logout") : setBtnName("Login") 
+                        }}
+                        className="login"
+                    >{btnName}</button>
                 </ul>
             </div>
         </div>
